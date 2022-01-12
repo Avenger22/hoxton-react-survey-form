95644fb76f24feb1c6f6cbf0035f0c1f7d716072
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-01-12 14:37:35 +0100

    first commit

---
+++ src/components/AnswersList.jsx
@@ -0,0 +1,17 @@
+import AnswersItem from "./AnswersItem";
+
+export default function AnswersList({ answersList }) {
+
+  return (
+
+    <ul>
+
+      {answersList.map((answerItem, i) => (
+        <AnswersItem answerItem={answerItem} key={i} />
+      ))}
+      
+    </ul>
+
+  )
+
+}
